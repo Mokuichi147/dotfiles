99c15318709b495fdd6c9d12f9f694f1e0f37423
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2021-05-04 19:27:08 +0000

    Update zsh

--- init.sh
+++ init.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# zsh shell
+printf "password:"
+read password
+echo "$password" | sudo -S apt install -y zsh
+
+chsh -s $(which zsh)
+
 # Python (Pyenv)
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+
+# Rust
+curl https://sh.rustup.rs -sSf | sh
