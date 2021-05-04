b520d66fc8447434b11e084b8fac8db6aee18aed
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2021-05-04 20:17:27 +0000

    Update printf

--- init.sh
+++ init.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # hostname
-printf "hostname:"
-read hostname
-printf "password:"
-read -s password
+read -p "Hostname: " hostname
+read -sp "Password: " password
 echo "$password" | sudo -S hostnamectl set-hostname "$hostname"
+
 
 # zsh shell
 echo "$password" | sudo -S apt install -y zsh
-
 chsh -s $(which zsh)
+
 
 # Python (Pyenv)
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+
 
 # Rust
 curl https://sh.rustup.rs -sSf | sh
