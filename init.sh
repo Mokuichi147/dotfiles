48420fed6169254ac340e31d1fcdb6cd58c38ee6
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2021-05-04 20:21:13 +0000

    Update curl

--- init.sh
+++ init.sh
@@ -11,6 +11,13 @@ echo "$password" | sudo -S apt install -y zsh
 chsh -s $(which zsh)
 
 
+# zshrc & tmux.conf
+curl -sSf -o ~/dotfiles/.zshrc https://raw.githubusercontent.com/Mokuichi147/dotfiles/main/.zshrc
+curl -sSf -o ~/dotfiles/.tmux.conf https://raw.githubusercontent.com/Mokuichi147/dotfiles/main/.tmux.conf
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+
 # Python (Pyenv)
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 git clone https://github.com/pyenv/pyenv-update.git ~/.pyenv/plugins/pyenv-update
